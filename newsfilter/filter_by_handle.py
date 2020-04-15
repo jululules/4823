d5b08318b70a59589c7daa9b4840fb7b7d9f8122
Jules Henry <julesaaronhenry@gmail.com>
2020-04-15 13:55:09 -0400

    updated filter by handle to collect all screen_names for a whole day worth of tweets

--- newsfilter/filter_by_handle.py
+++ newsfilter/filter_by_handle.py
@@ -2,17 +2,37 @@ import sys;
 import json;
 
 
+def get_news_handles(filename):
+    keyword = "news"
+    screen_name_list = []
+    try:
+        line_generator = open(filename)
+    except:
+        print("no file named " + filename.__str__())
+        return
+    for line in line_generator:
+        line_object = json.loads(line)
+        username_string = line_object["user"]["screen_name"]
+        if keyword in username_string.lower():
+            screen_name_list.append(username_string)
+            print(username_string)
+    return screen_name_list
 
-keyword = "news"
 
-if len(sys.argv)>1:
-    line_generator = open(sys.argv[1])
-else:
-    line_generator = open("tweets_coronavirus_en_2020/03/07/14/30.json")
+def get_handles_on_day():
+    fname = "tweets_coronavirus_en_2020/03/01/"
+    for x in range(24):
+        if x < 10:
+            fopen = fname + "0" + x.__str__() + "/"
+        else:
+            fopen = fname + x.__str__() + "/"
+        for x in range(60):
+            if x < 10:
+                file = fopen + "0" + x.__str__() + ".json"
+            else:
+                file = fopen + x.__str__() + ".json"
+            get_news_handles(file)
+            #print(file)
 
-for line in line_generator:
-    line_object = json.loads(line)
-    username_string = line_object["user"]["screen_name"]
-    if keyword in username_string.lower():
-        print(username_string)
 
+get_handles_on_day()
